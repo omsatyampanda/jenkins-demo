27fc1fd4e29ace082ce6729cb884fb9deafe923e
omsatyampanda <35395341+omsatyampanda@users.noreply.github.com>
2025-08-28 22:52:23 +0530

    Update reportGenerator.groovy

--- shared-lib/vars/reportGenerator.groovy
+++ shared-lib/vars/reportGenerator.groovy
@@ -1,13 +1,16 @@
+// Exposed as: reportGenerator.generate(stages, jobName, buildNumber)
 def generate(List stages, String jobName, String buildNumber) {
   def rows = stages.collect { s ->
+    def seconds = (s.ms / 1000.0).toString()
     """<tr>
          <td>${s.name}</td>
          <td>${s.result}</td>
-         <td>${s.ms / 1000.0}s</td>
+         <td>${seconds}s</td>
          <td>${s.comment ?: '-'}</td>
        </tr>"""
   }.join("\n")
 
+  // Load template from resources
   def tmpl = libraryResource('report-template.html')
   return tmpl
     .replace('@@TITLE@@', "Build Report - ${jobName} #${buildNumber}")
@@ -15,6 +18,7 @@ def generate(List stages, String jobName, String buildNumber) {
     .replace('@@ROWS@@', rows)
 }
 
+// Allow calling as reportGenerator(stages, job, build)
 def call(List stages, String jobName, String buildNumber) {
   return generate(stages, jobName, buildNumber)
 }
